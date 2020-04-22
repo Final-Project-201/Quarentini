86df7751f278fdcc44c3929de662bd69702aeac8
lindseyshepard <lindsey.shepard@outlook.com>
2020-04-22 15:21:07 -0700

    lindsey commits

--- functions.js
+++ functions.js
@@ -10,10 +10,9 @@ function CreateQuestion(ask) {
     questionArray.push(this);
 }
 
-function CreateAnswers(imgsrc, name) {
-    this.name = name;
+function CreateAnswers(imgsrc, beerName) {
+    this.beerName = beerName;
     this.imgsrc = imgsrc;
-    this.class = '.imgclass';
     this.voteCount = 0;
     answersArray.push(this);
 }
@@ -73,7 +72,8 @@ function clickHandler(event) {
         for (var i = 0; i < answersArray.length; i++) {
 
             if (event.target.id === answersArray[i].name) {
-                answersArray[i].voteCount++;
+                // answersArray[i].voteCount++;
+                if (answersArray[i].beerName)
             }
 
             console.log('target', event.target.id);
@@ -92,55 +92,55 @@ function clickHandler(event) {
 var question1 = new CreateQuestion('Question 1');
 var question2 = new CreateQuestion('Question 2');
 var question3 = new CreateQuestion('Question 3');
-// var question4 = new CreateQuestion('Question 4');
-// var question5 = new CreateQuestion('Question 5');
-// var question6 = new CreateQuestion('Question 6');
-// var question7 = new CreateQuestion('Question 7');
-// var question8 = new CreateQuestion('Question 8');
-// var question9 = new CreateQuestion('Question 9');
-// var question10 = new CreateQuestion('Question 10');
+var question4 = new CreateQuestion('Question 4');
+var question5 = new CreateQuestion('Question 5');
+var question6 = new CreateQuestion('Question 6');
+var question7 = new CreateQuestion('Question 7');
+var question8 = new CreateQuestion('Question 8');
+var question9 = new CreateQuestion('Question 9');
+var question10 = new CreateQuestion('Question 10');
 
 
-var answer1 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameOne');
-var answer2 = new CreateAnswers('images/pexels-photo-1267257.jpeg', 'nameTwo');
-var answer3 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameFour');
-var answer4 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameThree');
-var answer5 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'namefive');
-var answer6 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'namesix');
-var answer7 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameseven');
-var answer8 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameeight');
-var answer9 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'namenine');
-var answer10 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameten');
-var answer11 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nameeleven');
-var answer12 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'nanetwelve');
-// var answer13 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer14 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer15 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer16 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer17 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer18 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer19 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer20 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer21 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer22 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer23 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer24 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer25 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer26 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer27 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer28 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer29 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer30 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer31 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer32 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer33 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer34 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer35 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer36 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer37 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer38 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer39 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
-// var answer40 = new CreateAnswers('images/your-moves-are-5b24a2.jpg');
+var answer1 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerOne');
+var answer2 = new CreateAnswers('images/pexels-photo-1267257.jpeg', 'beerOne');
+var answer3 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerOne');
+var answer4 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerOne');
+var answer5 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTwo');
+var answer6 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTwo');
+var answer7 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTwo');
+var answer8 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTwo');
+var answer9 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerThree');
+var answer10 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerThree');
+var answer11 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerThree');
+var answer12 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerThree');
+var answer13 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFour');
+var answer14 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFour');
+var answer15 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFour');
+var answer16 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFour');
+var answer17 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFive');
+var answer18 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFive');
+var answer19 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFive');
+var answer20 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerFive');
+var answer21 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSix');
+var answer22 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSix');
+var answer23 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSix');
+var answer24 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSix');
+var answer25 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSeven');
+var answer26 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSeven');
+var answer27 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSeven');
+var answer28 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerSeven');
+var answer29 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerEight');
+var answer30 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerEight');
+var answer31 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerEight');
+var answer32 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerEight');
+var answer33 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerNine');
+var answer34 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerNine');
+var answer35 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerNine');
+var answer36 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerNine');
+var answer37 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTen');
+var answer38 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTen');
+var answer39 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTen');
+var answer40 = new CreateAnswers('images/your-moves-are-5b24a2.jpg', 'beerTen');
 
 
 
